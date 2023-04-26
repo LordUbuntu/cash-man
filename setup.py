6328d485ea501d9d5d30b34947d084fbb116021f
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-25 17:57:50 -0700

    improved setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
-import setuptools
+from setuptools import setup, find_packages
 
 
 with open("README.md", 'r') as f:
     long_description = f.read()
 
 
-setuptools.setup(
+setup(
     name="cashman",
     version="0.0.0",  # SemVer... Major, Minor, Patch
-    description="track personal expenses",
+    description="A cli tool to track personal expenses",
     long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LordUbuntu/cash-man",
     license="Unlicense",
     author="Jacobus Burger",
     author_email="therealjacoburger@gmail.com",
-    packages=["cashman"],
+    packages=find_packages(),
     install_requires=["rich==13.3.4", "click==8.1.3"],
 )
