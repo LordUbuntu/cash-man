d0335dbc8a9420b6ca9e3f4e39017e4aa3f9d740
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-25 18:56:33 -0700

    Hello, Worldgit status

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 with open("README.md", 'r') as f:
@@ -16,17 +16,23 @@ setup(
     license="Unlicense",
     author="Jacobus Burger",
     author_email="therealjacoburger@gmail.com",
-    packages=find_packages(),
+    packages=["cashman"],
     install_requires=["rich==13.3.4", "click==8.1.3"],
     extras_require={
         "dev": ["pytest>=7.2", "twine>=4.0.2"],
     },
     python_requires=">=3.10",
+    platforms=["any"],
+    py_modules=["cashman"],
+    entry_points={
+        "console_scripts": ["cashman=cashman.__main__:main"],
+    },
     classifiers=[
-        "Development Status :: Planning",
+        "Development Status :: 1 - Planning",
         "License :: Unlicense",
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
-        'Topic :: Utilities',
+        "Topic :: Utilities",
+        "Environment :: Console",
     ],
 )
