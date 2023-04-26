2294f1185942de546d6c78caeb8af9686c98a707
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-25 18:14:42 -0700

    added more info

--- setup.py
+++ setup.py
@@ -12,9 +12,21 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/LordUbuntu/cash-man",
+    keywords=["python", "expense tracker", "personal finance", "finance", "cli"]
     license="Unlicense",
     author="Jacobus Burger",
     author_email="therealjacoburger@gmail.com",
     packages=find_packages(),
     install_requires=["rich==13.3.4", "click==8.1.3"],
+    extras_require={
+        "dev": ["pytest>=7.2", "twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
+    classifiers=[
+        "Development Status :: Alpha",
+        "License :: Unlicense",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+        'Topic :: Utilities',
+    ],
 )
