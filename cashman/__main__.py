64b3723635f279923fdee01beaa40e632563cdd9
LordUbuntu <therealjacoburger@gmail.com>
2023-06-22 11:14:25 -0700

    began work on better pandas filter-switching

--- cashman/__main__.py
+++ cashman/__main__.py
@@ -1,7 +1,45 @@
 # main cashman executable
 from datetime import date
 import click
+<<<<<<< Updated upstream
 # TODO might switch to argparse since I don't want users to have to put a -- at the end to record a negative transaction, though I might be able to get away with using a different subcommand for adding or subtracticing a transaction, though that seems painfully repetitive and overcomplicated.
+=======
+
+
+DATA_PATH = "{}/.local/share/cashman".format(os.path.expanduser('~'))
+if not os.path.exists(DATA_PATH):
+    os.makedirs(DATA_PATH)
+
+>>>>>>> Stashed changes
+
+
+
+
+def store_dataframe(data: dict):
+    # get file and path
+    file = "{}.csv".format(data["Date"][0].year)
+    path = "{}/{}".format(DATA_PATH, file)
+    # create data_frame
+    data_frame = pd.DataFrame(data)
+    # add header if file exists, otherwise append data without header
+    data_frame.to_csv(
+            path,
+            mode='a',
+            index=False,
+            header=(not os.path.isfile(path))
+    )
+
+
+# TODO: filter out dataframes
+def get_dataframe(mask, year=date.year):
+    file = "{}.csv".format(year)
+    path = "{}/{}".format(DATA_PATH, file)
+    data_frame = pd.read_csv(path)
+    return data_frame.loc[mask]
+
+
+
+
 
 @click.group()
 def cashman():
@@ -40,11 +78,54 @@ def list(date):
 
     DATE is the YYYY-MM-DD date of transactions to list.
     """
+<<<<<<< Updated upstream
     print("Your transactions for {} are: []".format(date))
 
 
 def main():
     print("Hello, World! 你好世界！ Hola Mundo!")
+=======
+    date = date.date()  # because click gives me timestamps I don't want
+    data_frame = get_dataframe(date)
+    if data_frame.empty:
+        print("There were no transactions for {}".format(date))
+    else:
+        print("Your transactions for {} are: ".format(date))
+        print(data_frame)
+
+
+@cashman.command()
+@click.argument("date", type=click.DateTime(formats=['%Y-%m-%d']), default=str(datetime.date.today()))
+def net(date):
+    """Calculates the net of transactions for a given date.
+    
+    DATE is the YYYY-MM-DD date of transactions to list.
+    """
+    date = date.date()  # because click gives me timestamps I don't want
+    data_frame = get_dataframe(date)
+    if data_frame.empty:
+        print("There were no transactions for {}".format(date))
+    else:
+        total = data_frame["Amount"].sum()
+        print("Your net transactions for {} are: {}".format(date, total))
+
+
+# TODO: Rewrite read and write functions for dataframes to allow better
+# filtering and selection
+@cashman.command()
+@click.argument("start", type=click.DateTime(formats=['%Y-%m-%d']), default=str(datetime.date.today()))
+def diff(start, end):
+    """Calculates the net change between two dates.
+
+    DATE is the YYYY-MM-DD date of transactions to list.
+    """
+    start = start.date()
+    end = end.date()
+
+
+
+
+>>>>>>> Stashed changes
 
 
 if __name__ == '__main__':
