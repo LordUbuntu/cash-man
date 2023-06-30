39ae084487e52fb315cd4331f900917430e0beea
LordUbuntu <therealjacoburger@gmail.com>
2023-06-29 19:12:15 -0700

    reformat code

--- cashman/data.py
+++ cashman/data.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 import datetime
+
+
+
 # DATA
 DATA_PATH = "{}/.local/share/cashman".format(os.path.expanduser('~'))
 if not os.path.exists(DATA_PATH):
@@ -8,23 +11,20 @@ if not os.path.exists(DATA_PATH):
     os.makedirs(DATA_PATH)
 
 
-def store_dataframe(data: dict):
+def put(data: dict):
     # get file and path
     file = "{}.csv".format(data["Date"][0].year)
     path = "{}/{}".format(DATA_PATH, file)
     # create data_frame
     data_frame = pd.DataFrame(data)
     # add header if file exists, otherwise append data without header
-    data_frame.to_csv(
-            path,
-            mode='a',
-            index=False,
-            header=(not os.path.isfile(path))
-    )
+    data_frame.to_csv(path,
+                      mode='a',
+                      index=False,
+                      header=(not os.path.isfile(path)))
 
 
-# TODO: filter out dataframes
-def get_dataframe(mask, year=datetime.date().year):
+def get(mask=True, year=datetime.date().year):
     file = "{}.csv".format(year)
     path = "{}/{}".format(DATA_PATH, file)
     data_frame = pd.read_csv(path)
